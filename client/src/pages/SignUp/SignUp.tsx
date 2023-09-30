de7a4556f5623b47e2901f9b37d78ce5492adf9d
Calicomp1-1 <113641798+Calicomp1-1@users.noreply.github.com>
2023-09-30 18:05:38 +0000

    Added Some text fields to sign up page

--- client/src/pages/SignUp/SignUp.tsx
+++ client/src/pages/SignUp/SignUp.tsx
@@ -7,6 +7,10 @@ function SignUp() {
       <div className="signup-page">
         <h1>Sign Up</h1>
       </div>
+      <p className="signup-page"> Create Email: </p>
+      <input type="text" id="SignUpEmail" name="SignUpEmail" /><br></br><br></br>
+      <p className="signup-page"> Create Password: </p>
+      <input type="text" id="SignUpPassword" name="SignUpPassword" /><br></br>
     </Page>
   );
 }
